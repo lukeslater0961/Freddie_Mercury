39da56659777da35261ec11b86821b30c3e78292
Luke Slater <luke.a.slater@outlook.com>
2026-01-29 10:48:57 +0100

    fixed sarcophagus movement on interact. stilll need to make it poke interact and not simple

--- Freddie_mercury/Assets/Scripts/World_Objects/Sarcophagus.cs
+++ Freddie_mercury/Assets/Scripts/World_Objects/Sarcophagus.cs
@@ -2,9 +2,14 @@ using UnityEngine;
 
 public class Sarcophagus : Interactible
 {
+	[SerializeField]	Rigidbody	lid;
+
+
+	[ContextMenu ("interact with Sarcophagus")]
 	public override void OnSelect()
 	{
 		Debug.Log("sarcophargus interacted with");
-		//trigger animation
+		lid.isKinematic = false;
+		Debug.Log($"rb is kinematic {lid.isKinematic}");
 	}
 }
